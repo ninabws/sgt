1b9377247174ad333fdf09a1c69531502c9edcb1
ninabws <nina.bianca06@aluno.ifce.edu.br>
2023-03-14 16:58:38 -0300

    .

--- sgt/sgt/settings.py
+++ sgt/sgt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tasks",
     'hello',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ sgt/tasks/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+tasks = ["foo", "bar", "baz"]
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+    
+def add(request):
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
+            request.session["tasks"] += [task]
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
+
+
